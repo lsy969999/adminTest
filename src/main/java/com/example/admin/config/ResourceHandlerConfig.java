bccb6f9e1f6bc41cb0a761d6b17b589c4b0212d7
seongyun.lim <seongyun.lim@e4net.net>
2023-12-13 12:31:32 +0900

    thymeleaf webcomponent test

---
+++ src/main/java/com/example/admin/config/ResourceHandlerConfig.java
@@ -0,0 +1,14 @@
+package com.example.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceHandlerConfig implements WebMvcConfigurer{
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/public/**")
+            .addResourceLocations("classpath:/static/");
+  }
+}
--- src/main/java/com/example/admin/controller/SampleController.java
+++ src/main/java/com/example/admin/controller/SampleController.java
@@ -1,12 +1,15 @@
 package com.example.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SampleController {
   @GetMapping
-  public String hi(){ 
+  public String hi(Model model){ 
+    model.addAttribute("test", "test");
+    model.addAttribute("name", "Lsy");
     return "pages/test";
   }
 }
